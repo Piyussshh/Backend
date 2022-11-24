b6203fdb61c5a787ba7ec4d08dc1c8fb6fe6cbdc
piyushgupta08 <piyush.gupta08@nagarro.com>
2022-11-24 18:36:35 +0530

    initial commit

---
+++ config/env.js
@@ -0,0 +1,10 @@
+
+const env = {
+  database: "azure",
+  username: "Piyush_39",
+  password: "Atharv@12345",
+  host: "myserver1.mysql.database.azure.com",
+  dialect: "mysql",
+};
+
+module.exports = env;
